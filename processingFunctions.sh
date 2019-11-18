12030491be061da7c3811c30238ee729dbbcc70e
gwiedeman <gregory.wiedeman1@gmail.com>
2019-11-18 09:49:48 -0500

    updated shortcut functions to log to log subdirectory

--- processingFunctions.sh
+++ processingFunctions.sh
@@ -4,11 +4,15 @@ ingest () {
     # function to call ingest.py in the background
 
     args="$*"
+    colID="$(cut -d'_' -f1 <<<"$args")"
+    
+    # make log dir if it doesn't exist
+    [[ -d /media/SPE/ingest/log/"$colID" ]] || mkdir /media/SPE/ingest/log/"$colID"
 
     # set pyenv
     #pyenv activate ingest
-    sudo -b python3 /opt/lib/ingest-processing-workflow/ingest.py $args >> /media/SPE/ingest/"$1"-ingest.log 2>&1
-    
+    sudo -b python3 /opt/lib/ingest-processing-workflow/ingest.py $args >> /media/SPE/ingest/ingest-"$1".log 2>&1
+
     # deactivate pyenv
     #pyenv deactivate
 }
@@ -17,11 +21,15 @@ transfer () {
     # function to call transferAccession.py in the background
 
     args="$*"
+    colID="$(cut -d'_' -f1 <<<"$args")"
+    
+    # make log dir if it doesn't exist
+    [[ -d /media/SPE/ingest/log/"$colID" ]] || mkdir /media/SPE/ingest/log/"$colID"
 
     # set pyenv
     #pyenv activate ingest
-    sudo -b python3 /opt/lib/ingest-processing-workflow/transferAccession.py $args >> /media/SPE/ingest/"$1"-transfer.log 2>&1
-    
+    sudo -b python3 /opt/lib/ingest-processing-workflow/transferAccession.py $args >> /media/SPE/ingest/transfer-"$1".log 2>&1
+
     # deactivate pyenv
     #pyenv deactivate
 }
@@ -30,11 +38,15 @@ packageAIP () {
     # function to call packageAIP.py in the background
 
     args="$*"
+    colID="$(cut -d'_' -f1 <<<"$args")"
+    
+    # make log dir if it doesn't exist
+    [[ -d /media/SPE/processing/log/"$colID" ]] || mkdir /media/SPE/processing/log/"$colID"
 
     # set pyenv
     #pyenv activate ingest
-    sudo -b python3 /opt/lib/ingest-processing-workflow/packageAIP.py $args >> /media/SPE/processing/"$1"-packageAIP.log 2>&1
-    
+    sudo -b python3 /opt/lib/ingest-processing-workflow/packageAIP.py $args >> /media/SPE/processing/log/packageAIP-"$1".log 2>&1
+
     # deactivate pyenv
     #pyenv deactivate
 }
