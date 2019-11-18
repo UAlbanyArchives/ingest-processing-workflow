1a088305d8de2e176e4aeade5ae891a89d12965d
gwiedeman <gregory.wiedeman1@gmail.com>
2019-11-18 14:13:49 -0500

    fixed log path and added timestamps

--- processingFunctions.sh
+++ processingFunctions.sh
@@ -4,14 +4,10 @@ ingest () {
     # function to call ingest.py in the background
 
     args="$*"
-    colID="$(cut -d'_' -f1 <<<"$args")"
     
-    # make log dir if it doesn't exist
-    [[ -d /media/SPE/ingest/log/"$colID" ]] || mkdir /media/SPE/ingest/log/"$colID"
-
     # set pyenv
     #pyenv activate ingest
-    sudo -b python3 /opt/lib/ingest-processing-workflow/ingest.py $args >> /media/SPE/ingest/ingest-"$1".log 2>&1
+    sudo -b python3 /opt/lib/ingest-processing-workflow/ingest.py $args >> /media/SPE/ingest/log/$(date +"%Y%m%d%H%M")-ingest-"$1".log 2>&1
 
     # deactivate pyenv
     #pyenv deactivate
@@ -21,14 +17,10 @@ transfer () {
     # function to call transferAccession.py in the background
 
     args="$*"
-    colID="$(cut -d'_' -f1 <<<"$args")"
-    
-    # make log dir if it doesn't exist
-    [[ -d /media/SPE/ingest/log/"$colID" ]] || mkdir /media/SPE/ingest/log/"$colID"
 
     # set pyenv
     #pyenv activate ingest
-    sudo -b python3 /opt/lib/ingest-processing-workflow/transferAccession.py $args >> /media/SPE/ingest/transfer-"$1".log 2>&1
+    sudo -b python3 /opt/lib/ingest-processing-workflow/transferAccession.py $args >> /media/SPE/ingest/log/$(date +"%Y%m%d%H%M")-transfer-"$1".log 2>&1
 
     # deactivate pyenv
     #pyenv deactivate
@@ -45,7 +37,7 @@ packageAIP () {
 
     # set pyenv
     #pyenv activate ingest
-    sudo -b python3 /opt/lib/ingest-processing-workflow/packageAIP.py $args >> /media/SPE/processing/log/packageAIP-"$1".log 2>&1
+    sudo -b python3 /opt/lib/ingest-processing-workflow/packageAIP.py $args >> /media/SPE/processing/log/"$colID"/$(date +"%Y%m%d%H%M")-packageAIP-"$1".log 2>&1
 
     # deactivate pyenv
     #pyenv deactivate
